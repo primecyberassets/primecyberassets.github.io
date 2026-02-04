f6249a0ae75a1407814ebf4ca9f50fb0262661a5
primecyberassets <primecyberassets@gmail.com>
2026-02-04 10:55:07 -0700

    Create sw.js

---
+++ apps/fibercounter/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "fibercounter-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+// Install: cache core assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: take control immediately
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch: serve cached first, then network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
